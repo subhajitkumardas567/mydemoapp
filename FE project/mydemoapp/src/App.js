3cc6ba46dd7b904ee7ba67b28435e3ecdb5ac7cd
Subhajit Kumar Das <dassubhajit695@gmail.com>
2026-03-15 10:55:56 +0530

    feature: desciption added

--- FE project/mydemoapp/src/App.js
+++ FE project/mydemoapp/src/App.js
@@ -1,18 +1,24 @@
+import { useState } from 'react';
 import Imagecard from './components/Imagecard.js';
 import './App.css';
+import { userData } from './components/constants/constant.js';
 
 function App() {
-  const userData=[
-    {name: "Subhajit Kumar Das", githubId:  "172288926"}
-  ];
+  const [showFlag,setShowFlag]=useState(false);
+  const clickHandler=()=>{
+    showFlag ? setShowFlag(false) : setShowFlag(true);//setshowflag is initially false
+  }
+
   return (
     <div className="App">
       {
         userData.map((user)=>(  
-          <>
-          <Imagecard key={user.githubId} name= {user.name} imglink={`https://avatars.githubusercontent.com/u/${user.githubId}?v=4`}/> 
-          <h3>Student</h3> 
-          </>
+          <div key={user.githubId}>
+          <Imagecard name= {user.name} imglink={`https://avatars.githubusercontent.com/u/${user.githubId}?v=4`}/> 
+          {/*conditional rendering*/}
+          <h3 onClick={clickHandler}>{user.isStudent?"Student":"Mentor"}⬇️⬇️⬇️</h3> 
+          {showFlag ?<p>{user.bioDescription}</p>:null}
+          </div>
         ))
       }
       
---
+++ FE project/mydemoapp/src/components/constants/constant.js
@@ -0,0 +1,3 @@
+export const userData=[
+    {name: "Subhajit Kumar Das", githubId:  "172288926", isStudent:true, bioDescription: "subhajit is very good boy who likes to improve his programming skills"}
+  ];
