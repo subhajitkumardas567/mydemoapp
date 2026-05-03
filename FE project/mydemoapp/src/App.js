f3b004f13e929623e61da566c76269bdc80c978c
Subhajit Kumar Das <dassubhajit695@gmail.com>
2026-05-03 14:12:02 +0530

    FEAT: Implemented React Router for navigation between Home, About, and Contact pages. Updated Navbar component to use Link for routing. Added new pages for Home, About, and Contact with basic content.

--- FE project/mydemoapp/src/App.js
+++ FE project/mydemoapp/src/App.js
@@ -1,15 +1,24 @@
 //import Imagecard from './components/Imagecard.js';
 import './App.css';
 
-import Todolist from './components/Todolist.js';
-
-
+//import Todolist from './components/Todolist.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.js';
+import About from './pages/About.js';
+import Contact from './pages/Contact.js';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-      
-      <Todolist/>
-    </div>
+    <BrowserRouter> 
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ FE project/mydemoapp/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Home =()=>{
+    return(
+        <>
+        <h1>this is the Home page</h1>
+        </>
+    );
+    };
+
+    export default Home;
